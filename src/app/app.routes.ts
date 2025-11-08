168cad163b790556688c50ab37944f7842f42e06
lucascriado <lucascriado33@gmail.com>
2025-11-08 13:35:55 -0300

    feat: rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Storage } from './storage/storage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'storage', component: Storage }
+];
---
+++ src/app/storage/storage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storage',
+  imports: [],
+  templateUrl: './storage.html',
+  styleUrl: './storage.css',
+})
+export class Storage {
+
+}
