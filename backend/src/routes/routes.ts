336f46c0c0468c0cba67fa3504f44e5dd3c8cdfe
lucascriado <lucascriado33@gmail.com>
2026-01-18 11:05:53 -0300

    remove: frontend folders

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import usersController from '../controllers/usersController';
-import vehiclesController from '../controllers/vehiclesController';
 import Auth from "../middlewares/auth";
 
 const router = Router();
@@ -14,15 +13,5 @@ router.get("/usuarios/encontrar-usuario", usersController.FindUser);
 router.post("/usuarios/cadastrar", usersController.Register)
 
 router.post("/usuarios/entrar", usersController.Login);
-
-// vehicles
-
-router.post("/veiculos/marcas", vehiclesController.CriarMarca)
-
-router.post("/veiculos/modelos", vehiclesController.CriarModelo)
-
-router.post("/veiculos/cores", vehiclesController.CriarCor)
-
-router.post("/veiculos/veiculos", vehiclesController.CriarVeiculo)
 
 export default router;
